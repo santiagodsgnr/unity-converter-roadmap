d2ad79ac59b0fb129e50f062f62e9314d92ddc06
santiagodsgnr <santiagoardila1993@gmail.com>
2025-03-08 18:11:51 -0500

    backend(): aud URL vercel

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,13 @@ import converterRoutes from "./routes/conversion.routes.js"
 const app = express()
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: [
+        'http://localhost:5173', 
+        'http://127.0.0.1:5173',
+        'https://unity-converter-roadmap-wxz9.vercel.app'
+    ],
     credentials: true
-}))
+}));
 app.use(express.json())
 app.use('/api', converterRoutes)
 
