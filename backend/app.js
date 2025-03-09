077513d0047c49220c9079630b301340612919be
santiagodsgnr <santiagoardila1993@gmail.com>
2025-03-08 19:13:39 -0500

    ok ok

--- backend/app.js
+++ backend/app.js
@@ -6,15 +6,22 @@ import converterRoutes from "./routes/conversion.routes.js"
 
 const app = express()
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://unity-converter-roadmap-wxz9.vercel.app"
+  ];
+
 app.use(cors({
     origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    }
-}));
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  }));
 app.use(express.json())
 app.use('/api', converterRoutes)
 
